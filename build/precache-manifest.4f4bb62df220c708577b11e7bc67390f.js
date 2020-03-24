ed1eb1ff45d01b0cc79269fd07f391afa8b90ea1
Ramiro <ramiii.92@hotmail.com>
2020-03-24 18:31:31 -0300

    build 1.0

---
+++ build/precache-manifest.4f4bb62df220c708577b11e7bc67390f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1e6348fa5abafcdb004ba514130dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8771da255691664b02c",
+    "url": "/static/css/main.aa19a169.chunk.css"
+  },
+  {
+    "revision": "9be793bf390d866bbdba",
+    "url": "/static/js/2.f538df6a.chunk.js"
+  },
+  {
+    "revision": "e01a6be4704fce1f358cece65ad606d2",
+    "url": "/static/js/2.f538df6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8771da255691664b02c",
+    "url": "/static/js/main.46ee3fb7.chunk.js"
+  },
+  {
+    "revision": "3a4550ebbd190742f99f",
+    "url": "/static/js/runtime-main.c07f7967.js"
+  }
+]);
